bd6b6c245b8ff6a19c687785509670cf40abb1a9
Sergio Genovese <sgenovese@gmail.com>
2022-12-21 22:00:36 -0300

    pepe

--- src/script.js
+++ src/script.js
@@ -1,49 +1,49 @@
   
  function inicializar(){
   // Las etiquetas son las porciones de la gráfica
-var etiquetas = ["C/C++", "VB/.Net","Assembler","HTML/CSS/JavaScript"]
-// Podemos tener varios conjuntos de datos. Comencemos con uno
-var titulo ="Lenguajes de Programación"
-var datosIngresos = {
-  data: [35, 35, 10, 15], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
-  // Ahora debería haber tantos background colors como datos, es decir, para este ejemplo, 4
-  backgroundColor: [
-    'red',
-    'green',
-    'blue',
-    'orange',
-],// Color de fondo
-  borderColor: [
-    'red',
-    'green',
-    'blue',
-    'orange',
-],// Color del borde
-  borderWidth: 0,// Ancho del borde
-};
-grafico_Donut(datosIngresos, etiquetas, titulo, "#donut-chart1");
-
-// Las etiquetas son las porciones de la gráfica
-var etiquetas = ["Escritura", "Hablado","Lectura"]
-// Podemos tener varios conjuntos de datos. Comencemos con uno
-var titulo ="Ingles Técnico"
-var datosIngresos = {
-  data: [10, 10, 80], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
-  // Ahora debería haber tantos background colors como datos, es decir, para este ejemplo, 4
-  backgroundColor: [
-    'red',
-    'green',
-    'blue',
-],// Color de fondo
-  borderColor: [
-    'red',
-    'green',
-    'blue',
-],// Color del borde
-  borderWidth: 0,// Ancho del borde
-};
-grafico_Donut(datosIngresos, etiquetas, titulo, "#donut-chart2");
-datos();
+  var etiquetas = ["C/C++", "VB/.Net","Assembler","HTML/CSS/JavaScript"]
+  // Podemos tener varios conjuntos de datos. Comencemos con uno
+  var titulo ="Lenguajes de Programación"
+  var datosIngresos = {
+    data: [35, 35, 10, 15], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+    // Ahora debería haber tantos background colors como datos, es decir, para este ejemplo, 4
+    backgroundColor: [
+      'red',
+      'green',
+      'blue',
+      'orange',
+  ],// Color de fondo
+    borderColor: [
+      'red',
+      'green',
+      'blue',
+      'orange',
+  ],// Color del borde
+    borderWidth: 0,// Ancho del borde
+  };
+  grafico_Donut(datosIngresos, etiquetas, titulo, "#donut-chart1");
+  
+  // Las etiquetas son las porciones de la gráfica
+  var etiquetas = ["Escritura", "Hablado","Lectura"]
+  // Podemos tener varios conjuntos de datos. Comencemos con uno
+  var titulo ="Ingles Técnico"
+  var datosIngresos = {
+    data: [10, 10, 80], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+    // Ahora debería haber tantos background colors como datos, es decir, para este ejemplo, 4
+    backgroundColor: [
+      'red',
+      'green',
+      'blue',
+  ],// Color de fondo
+    borderColor: [
+      'red',
+      'green',
+      'blue',
+  ],// Color del borde
+    borderWidth: 0,// Ancho del borde
+  };
+  grafico_Donut(datosIngresos, etiquetas, titulo, "#donut-chart2");
+  datos();
 document.getElementById('btn_Login').innerText="Login";
 ocultar_Botones();
 iniciar_Lista();
